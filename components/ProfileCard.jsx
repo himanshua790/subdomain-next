15b651939dd58c97b9fcc21fc55bcea5944153c3
Himanshu Soni <himanshua790@gmail.com>
2023-10-25 00:30:26 +0530

    chore: minor changes

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const ProfileCard = async ({ profile }) => {
-  console.log(profile?.styles?.ProfileCard);
   return (
     <div style={profile?.styles?.ProfileCard || {}}>
       <h2>{profile?.name}</h2>
